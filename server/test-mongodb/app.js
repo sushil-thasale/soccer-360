926c00050b448750ffa40401b35e09236c1cd7c4
sushil-thasale <sushil.thasale@gmail.com>
2017-09-13 00:06:31 -0400

    updating mongoose connection code

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -9,10 +9,22 @@ module.exports = function(app)
     app.delete("/api/test/:id", deleteMessage);
 
 
-  var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
+  // var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
 
+  var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+
+  if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds111111.mlab.com:11111/databasename'; // use yours
+  }
+
+  // updates to mongoose
   var mongoose = require("mongoose");
-    mongoose.connect(connectionString);
+    mongoose.connect(connectionString, {
+      useMongoClient: true
+  });
 
     var TestSchema = mongoose.Schema({
         message: String
